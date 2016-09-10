e454fb94acbac724dee752d0c4c6e3f5fa585438
airqj <gugeqinjianbo@gmail.com>
2016-09-10 20:48:06 +0800

    parse all file for max data length

--- src/main/scala-2.11/Test.scala
+++ src/main/scala-2.11/Test.scala
@@ -7,9 +7,13 @@ import org.json4s._
 import org.json4s.native.JsonMethods._
 import scalikejdbc._
 import java.io.File
+import java.io.PrintWriter
 
 object Test {
   def main(args: Array[String]): Unit = {
+
+    val newDataPath = "/home/qjb/DATA/totalDataPath/"
+
     case class userData(zhihuId: String, topic: String, lineNumber: Long)
     Class.forName("org.postgresql.Driver")
     ConnectionPool.singleton("jdbc:postgresql://localhost:5432/zhihudb", "dbuser", "admin")
@@ -43,10 +47,23 @@ object Test {
             """.execute().apply()
     }
 
-    for (fileName <- new File("/home/qjb/DATA/topics1").listFiles) {
+    def whereIsData(zhihuId: String):(String,Long) = {
+      val res =
+        sql"""
+              SELECT * FROM userMaxDataLength WHERE zhihuId = $zhihuId
+          """.map(rs => (rs.string("topicname"),rs.long("linenumber"))).list().apply()
+
+      if(res.length == 1) res(0) else ("0",0)
+    }
+
+    val currentDataDir = "/home/qjb/DATA/topics/"
+    for (fileName <- new File(currentDataDir).listFiles) {
       val name = fileName.getName()
-      var numberIndex = 1
+//      var numberIndex = 1
+//      val newTopicName = new File(newDataPath+fileName.getName())
+      val pw = new PrintWriter(newDataPath + fileName.getName())
       for (line <- Source.fromFile(fileName).getLines()) {
+        /*
         val userDataLine = parse(line)
         val lineLength = line.length()
         val userId = (userDataLine \ "id").values.toString()
@@ -56,8 +73,17 @@ object Test {
         else insertUser(userId,name,numberIndex,lineLength)
 
         numberIndex += 1
+        */
+        val userId = (parse(line) \ "id").values.toString()
+        val dataLocation = whereIsData(userId)
+        if(dataLocation == ("0",0)) throw new RuntimeException("user not found")
+        val data = new File(currentDataDir + dataLocation._1)
+        val dataLine = Source.fromFile(currentDataDir + dataLocation._1).getLines().drop((dataLocation._2 - 1).toInt).next()
+        pw.write(dataLine + "\n")
+        pw.flush()
+        println("finished a line")
       }
-
+      pw.close()
     }
   }
 }
